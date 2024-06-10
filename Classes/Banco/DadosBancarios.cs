0d6b191bc4a71168fd9d51dc6035903e78bd2d60
Gabriel Matozinho <109115976+Gabriel-Matozinho@users.noreply.github.com>
2024-06-10 13:23:44 -0300

    Banco

---
+++ Classes/Banco/DadosBancarios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste {
+    internal class DadosBancarios {
+
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public DadosBancarios(int conta,string titular) {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public DadosBancarios(int conta,string titular,double saldo) : this(conta,titular) {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double deposito) {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque) {
+            Saldo = Saldo - saque - 5.00;
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + Conta
+                + ", Titular: "
+                + Titular
+                + ", Saldo: R$ "
+                + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
