e97f01cd8a62ecdc9717057858910e927571565b
Gabriel Matozinho <gabriel.intec@hotmail.com>
2024-06-13 08:32:37 -0300

    atualização

---
+++ Classes/Vetor_Classes/VetorClasse01/ProdutoVetor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorClasse01 {
+    internal class ProdutoVetor {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public ProdutoVetor(string nome,double preco) {
+            Nome = nome;
+            Preco = preco;       
+        }
+    }
+}
---
+++ Classes/Vetor_Classes/VetorClasse01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetorClasse01 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            int n = int.Parse(Console.ReadLine());
+            ProdutoVetor[] prod = new ProdutoVetor[n];
+
+            for(int i = 0; i < n; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                string nome = s[0];
+                double preco = double.Parse(s[1],CultureInfo.InvariantCulture);
+                prod[i] = new ProdutoVetor(nome,preco);
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                sum += prod[i].Preco;
+            }
+
+            double avg = 0.0;
+            for(int i = 0; i < n; i++) {
+                avg = sum / n;
+            }
+
+            Console.WriteLine(avg.ToString("F2",CultureInfo.InvariantCulture));
+        }
+
+        
+    }
+}
