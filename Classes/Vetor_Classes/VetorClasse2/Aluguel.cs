19df872ec7063ce2040ba954ce9a0c5d61c9ceb7
Gabriel Matozinho <gabriel.intec@hotmail.com>
2024-06-13 15:14:18 -0300

    atualização

--- Classes/Vetor_Classes/VetorClasse2/Aluguel.cs
+++ Classes/Vetor_Classes/VetorClasse2/Aluguel.cs
@@ -1,9 +1,11 @@
 ﻿
 namespace VetorClasse02 {
     internal class Aluguel {
+        //Declaração das propriedades
         public string Name { get; set; }
         public string Email { get; set; }      
 
+        //Construtor
         public Aluguel(string name,string email) {
             Name = name;
             Email = email;             
--- Classes/Vetor_Classes/VetorClasse2/Program.cs
+++ Classes/Vetor_Classes/VetorClasse2/Program.cs
@@ -2,11 +2,17 @@
     internal class Program {
         static void Main(string[] args) {
 
+
+            //Entrada do vetor já declarando a quantia de vagas de quartos
             Aluguel[] aluguel = new Aluguel[10];
+
+            //Entrada de dados para a quantia de vagas de quartos a serem alugados
             Console.Write("Quantos quartos serão alugados: ");
             int QuantiaDeQuartosAlugados = int.Parse(Console.ReadLine());
 
+            //Laço de repetição
             for(int i = 1; i <= QuantiaDeQuartosAlugados; i++) {
+                //Saída de dados
                 Console.WriteLine($"Rent #{i}");
                 Console.Write("Name: ");
                 string nome = Console.ReadLine();
@@ -14,13 +20,13 @@
                 string email = Console.ReadLine();
                 Console.Write("Quarto: ");
                 int quarto = int.Parse(Console.ReadLine());
-                aluguel[i] = new Aluguel(nome,email,quarto);
+                aluguel[i] = new Aluguel(nome,email);
             }
             Console.WriteLine();
             Console.WriteLine("Busy rooms");
             for(int i = 0; i < 10; i++) {
-                if(aluguel[i] != null) {
-                    Console.WriteLine(i + ": " + aluguel[i]);
+                if(aluguel[i] != null) {//Condicional para verificar dentro do vetor quais são as posições que não estão com valores nulos 
+                    Console.WriteLine(i + ": " + aluguel[i]); //Saida de dados com o override Tostring da classe 'aluguel'
                 }
             }
         }
