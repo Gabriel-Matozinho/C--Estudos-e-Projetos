2a17d855a8b76e13de3524510fdb5915c6a310ee
Gabriel Matozinho Pereira <gabriel.intec@hotmail.com>
2024-07-02 14:31:39 -0300

    Adiciona e atualiza arquivos de várias classes e módulos

--- Classes/Classe/Aluno_Classe/AlunoPrincipal/obj/Debug/net6.0/AlunoPrincipal.AssemblyInfo.cs
+++ Classes/Classe/Aluno_Classe/AlunoPrincipal/obj/Debug/net6.0/AlunoPrincipal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlunoPrincipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46152ad75c14a7369a81d54fb445ec5454d27fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlunoPrincipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlunoPrincipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
