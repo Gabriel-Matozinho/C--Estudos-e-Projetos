284c3f4520a86481f745db51c11a65114f8f2c7a
Gabriel Matozinho Pereira <gabriel.intec@hotmail.com>
2024-07-02 14:37:45 -0300

    Remove arquivos da pasta .vs e adiciona a pasta Enumerate

---
+++ Enumerate/Enumerate01/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Enumerate01.Entities.Enums
+{
+    enum OrderStatus
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipperd = 2,
+        Delivered = 3
+    }
+}
---
+++ Enumerate/Enumerate01/Program.cs
@@ -0,0 +1,21 @@
+﻿using Enumerate01.Entities;
+using System;
+using Enumerate01.Entities.Enums;
+
+namespace Enumerate01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order();
+            Console.Write("Digite o ID: ");
+            order.Id = int.Parse(Console.ReadLine());
+            order.Moment = DateTime.Now;
+            Console.Write("Status do pedido: ");
+            order.Status = (OrderStatus)Enum.Parse(typeof(OrderStatus),"Delivered");
+            Console.WriteLine(order);
+        }
+    }
+}
