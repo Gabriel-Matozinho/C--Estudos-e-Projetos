36b1adef60c10ede6dac5d625a4a620ae54565ac
Gabriel Matozinho Pereira <gabriel.intec@hotmail.com>
2024-07-15 16:48:43 -0300

    Atualização projeto order

---
+++ Enumerate/Projeto_Order/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_Order.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Order.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>(); 
+        
+    }
+}
---
+++ Enumerate/Projeto_Order/Program.cs
@@ -0,0 +1,14 @@
+﻿using Projeto_Order.Entities;
+using Projeto_Order.Entities.Enum;
+
+namespace Projeto_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+        }
+    }
+}
